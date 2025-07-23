4cfe114f7f6824b0814da21a7c7653063d52f4f2
Nzurumike Emmaunel chukwudi <92316410+c-empire@users.noreply.github.com>
2025-07-23 15:27:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const cors = require('cors');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
